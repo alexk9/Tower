74ab096ec2a416dfa76cad13469f630c86ae8ba3
alexk6 <ukira1@gmail.com>
2013-09-25 23:20:02 +0900

    GaurdTower와 Arrow 로 Sheep 을 쏘도록 수정함.
    기존 허접한 테스트 모델 제거
    근데 아직 여러가지로 어색한 동작임

---
+++ Assets/Scripts/GuardTower.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GuardTower : MonoBehaviour {
+
+	public Transform m_arrowTemplate;
+	public float FIRE_TERM = 2f;
+	public int NO =0;
+	private float m_accTerm;
+	
+	// Use this for initialization
+	void Start () {
+		//print ("SERTAR");
+		m_accTerm = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//print ("HI");
+		 if (Input.GetButtonDown("Fire1")) {
+	        GameObject spPoint = GameObject.Find("spawnPoint");                // spawnPoint 정보 읽기 -- ①
+	        //Instantiate(m_bullet, spPoint.transform.position, spPoint.transform.rotation);
+	        //myBullet.rigidbody.AddForce(spPoint.transform.forward * power);
+			//print ("FIRE!!!"+NO);
+	    }
+		
+		
+		if( m_accTerm < FIRE_TERM ){
+			m_accTerm += Time.deltaTime;
+		} else {
+			RaycastHit hit;
+			Vector3 fwd = Vector3.forward;
+			GameObject[] a_goTargets =  GameObject.FindGameObjectsWithTag("TARGET");
+			
+			for(int idx=0;idx<a_goTargets.Length;idx++){
+				//print(a_goTargets[idx]);
+				if( a_goTargets[idx].name.StartsWith("Target") == true){
+					//Guard Tower do not look at.
+					//transform.LookAt(a_goTargets[idx].transform);
+					Debug.DrawRay(transform.position,Vector3.forward,Color.red,5.0f);
+					//print("OBJECTS FOUNDS:["+a_goTargets[idx]+"]");
+					//Transform spPoint = gameObject.transform.FindChild("spawnPoint");                
+		        	Transform myArrow= (Transform)Instantiate(m_arrowTemplate, gameObject.transform.position, Quaternion.identity);
+					GuardTowerArrow gta = (GuardTowerArrow)myArrow.gameObject.GetComponent("GuardTowerArrow");
+					gta.setTarget(a_goTargets[idx]);
+		        	//myBullet.rigidbody.AddForce(spPoint.transform.forward * power);
+					//print ("FIRE!!!"+NO);
+					
+					break;
+				}
+			}
+			m_accTerm = 0;
+		}
+		Debug.DrawRay(transform.position,Vector3.forward,Color.red,5.0f);
+		
+		//if (Physics.Raycast(transform.position, fwd, hit, 20) == false) return;        // 탐색 실패
+	}
+}
---
+++ Assets/Scripts/GuardTowerArrow.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GuardTowerArrow : MonoBehaviour {
+	
+	private bool m_initialized = false;
+	private GameObject m_target ;
+	//private GameObject m_tower;
+	public float _speed=4;
+	private GameObject m_tempBodyObj;
+	
+	public void setTarget( GameObject target){
+		m_target = target;
+		m_initialized = true;
+	}
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if( m_initialized == true){
+			if( m_target != null){
+				
+				gameObject.transform.LookAt(m_target.transform);
+				//print ("GreenBar:"+m_target.transform.position+",Body:"+m_tempBodyObj.transform.position);
+				gameObject.transform.Translate(Vector3.forward*Time.deltaTime*_speed);
+			}else {
+				Destroy(gameObject);
+			}
+		}
+	}
+	
+	void OnTriggerEnter(Collider other){
+		print ("triggerEnter:"+other);
+		if( other.tag == "TARGET"){
+			Destroy(gameObject);
+		} else {
+			print ("ERROR!!!!!!!!!!!!!");
+		}
+	}
+	void OnTriggerExit(Collider other){
+		print ("triggerExit:"+other);
+		if( other.tag == "TARGET"){
+			Destroy(gameObject);
+		} else {
+			print ("ERROR!!!!!!!!!!!!!");
+		}
+	}
+}
